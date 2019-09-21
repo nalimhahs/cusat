adb00f6ca4ddef475df080571c40d63da3b25ef7
Milan Shah <milanshah1@gmail.com>
2019-09-21 19:44:55 +0530

    Updated models and migrations

--- order/models.py
+++ order/models.py
@@ -3,8 +3,34 @@ from user.models import CustomUser
 
 
 class Order(models.Model):
-    user_details = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name="User")
-    assigned_driver = models.ForeignKey(CustomUser, limit_choices_to={'user_type': CustomUser.DRIVER}, on_delete=models.PROTECT, related_name="Driver")
-    status = models.CharField(max_length=30)
+    user_details = models.ForeignKey(CustomUser, limit_choices_to={
+                                     'user_type': CustomUser.USER}, on_delete=models.PROTECT, related_name="User")
+    assigned_driver = models.ForeignKey(CustomUser, limit_choices_to={
+                                        'user_type': CustomUser.DRIVER}, on_delete=models.PROTECT, related_name="Driver")
+    
+    PLACED = 'P'
+    PICKED_UP = 'U'
+    TRANSIT = 'T'
+    RECIEVED = 'R'
+
+    STATUS_CHOICES = [
+        (PLACED, 'Placed'),
+        (PICKED_UP, 'Picked Up'),
+        (TRANSIT, 'In Transist'),
+        (RECIEVED, 'Recieved at collection center'),
+    ]
+    
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
     weight = models.IntegerField()
-    date_of_order = models.DateField()
+    date_of_order = models.DateField(auto_now_add=True)
+    waste_type = models.ForeignKey('WasteType', on_delete=models.PROTECT)
+
+
+
+class WasteType(models.Model):
+    name = models.CharField(max_length=30)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
 
     name = models.CharField(max_length=50)
@@ -26,7 +27,8 @@ class CustomUser(AbstractUser):
         default=USER,
     )
 
-    vehicle = models.ForeignKey('driver.Vehicle', on_delete=models.PROTECT)
+    vehicle = models.ForeignKey(
+        'driver.Vehicle', on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.email
