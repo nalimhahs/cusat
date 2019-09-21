afe06e8df6af3c43798082a86729ba96dd4a2100
karthika kumar <karthikapillai@gmail.com>
2019-09-21 16:50:38 +0530

    Collectioncentre model added

--- collectioncentre/models.py
+++ collectioncentre/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Collectioncentre(models.Model):
+    name=models.CharField(max_lenght=20)
+    location=models.Charfield(max_length=40):
+    Id=models.IntegerField():
+    
