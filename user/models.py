6f5262af1999da8bc55220778d283b9ddb5443d4
Milan Shah <milanshah1@gmail.com>
2019-09-21 16:12:50 +0530

    Modifying User model

--- user/models.py
+++ user/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
+class User(AbstractUser):
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=20, blank=True)
     phoneno = models.IntegerField()
     address = models.TextField(max_length=300)
     pincode = models.IntegerField()
     last_access = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
