3c3b34f677574b909a1fbf9838a9a784808ff6ae
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2019-09-21 23:13:23 +0530

    driver model added

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from .forms import *
 
 
 def get_user_data(request):
-    userdata = CustomUser.objects.filter(username=user.username)
+    userdata = CustomUser.objects.filter(username=request.user.username)
     if(CustomUser.user_type == 'U'):
         return render(request, '', {'userdata': userdata})
 
