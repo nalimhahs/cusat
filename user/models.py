4d205af0859c3897c7a05d68d40ed2e57975bbdf
Milan Shah <milanshah1@gmail.com>
2019-09-21 19:20:23 +0530

    Setup a few more models

--- user/models.py
+++ user/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class CustomUser(AbstractUser):
 
     name = models.CharField(max_length=50)
@@ -22,13 +21,12 @@ class CustomUser(AbstractUser):
     ]
 
     user_type = models.CharField(
-        max_length=2,
+        max_length=1,
         choices=USER_CHOICES,
         default=USER,
     )
 
-    
+    vehicle = models.ForeignKey('driver.Vehicle', on_delete=models.PROTECT)
 
-
-def __str__(self):
-    return self.email
+    def __str__(self):
+        return self.email
