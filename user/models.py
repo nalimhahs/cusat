a1a4ddf3bfff555ea393785291c65177cc0ca782
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2019-09-21 15:43:19 +0530

    added user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=40)
+    email=models.EmailField(max_length=20,blank=True)
+    phoneno=models.IntegerField()
+    address=models.TextField(max_length=300)
+    pincode=models.IntegerField()
+    lastaccess=models.DateTimeField(auto_now_add=True)
+    
