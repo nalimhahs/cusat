37bb0a260fe94be4b437643e3cd4be0af3dcc017
Milan Shah <milanshah1@gmail.com>
2019-09-21 15:45:11 +0530

    Added autopep8

--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+
+
 class User(models.Model):
-    name=models.CharField(max_length=40)
-    email=models.EmailField(max_length=20,blank=True)
-    phoneno=models.IntegerField()
-    address=models.TextField(max_length=300)
-    pincode=models.IntegerField()
-    lastaccess=models.DateTimeField(auto_now_add=True)
-    
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=20, blank=True)
+    phoneno = models.IntegerField()
+    address = models.TextField(max_length=300)
+    pincode = models.IntegerField()
+    last_access = models.DateTimeField(auto_now_add=True)
