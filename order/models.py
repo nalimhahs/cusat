93087b2b2060c0ac80ae091b6498b803598ecc4e
ASHIESH MATHEWS <ashieshmathews@gmail.com>
2019-09-21 17:17:26 +0530

    Order model added

--- order/models.py
+++ order/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
+from driver.models import Driver
+class Order(models.Model):
+    user_details=models.ForeignKey(User)
+    assigned_driver=models.ForeignKey(Driver)
+    status=models.CharField(max_length=30)
+    weight=models.IntegerField()
+    date_of_order=models.DateField()
 
-# Create your models here.
+
+
