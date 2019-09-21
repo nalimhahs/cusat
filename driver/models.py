9ecfd42c2109c7606f742a58a10913cd30570ffd
karthika kumar <karthikapillai@gmail.com>
2019-09-21 16:39:31 +0530

    Driver model added

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Driver(models.Model):
+    name=models.CharField(max_length=40)
+    email=models.EmailField(max_length=20,blank=True)
+    phoneno=models.IntegerField()
+    age=models.IntegerField()
+    vehiclecarrysize=models.IntegerField()
+    dimensions=models.DecimalField()
+    licenseno=models.IntegerField()
+    vehicleregno=models.IntegerField()
+    region=models.CharField(max_length=80)
+    totalearnings=models.DecimalField()
+
+    
